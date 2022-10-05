f2c9168b065f6332be3b66b40e140b47e886a454
HR0N <vivante.des@gmail.com>
2022-10-06 01:15:30 +0300

    customize little ^^

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -19,7 +19,7 @@ function Header() {
                             <div className="nav-item nav-home"><NavLink to={"/other/TEMPLATE/TEMPLATE"}  exact="true" end>item</NavLink></div>
                         </div>
                     </div>
-                    <div className="nav-item nav-home dropdown2"><NavLink className={'disable'} to={"/other/extra"} exact="true" end>Extra</NavLink>
+                    <div className="nav-item nav-home dropdown2"><NavLink className={'disable'} to={"/other/extra"} exact="true">Extra</NavLink>
                         <div className="nav-item nav-home dropdown-menu2">
                             <div className="nav-item nav-home"><NavLink to={"/other/extra/enchante_chance"}  exact="true" end>Шанс точки скилов</NavLink></div>
                         </div>
